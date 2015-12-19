e83fe22407cf9b83501152f85ba24250bdc32f5b
Peter Orum <peter@peterorum.com>
2015-12-19 15:07:14 +1100

    flux version

--- src/app/stores/random-store.js
+++ src/app/stores/random-store.js
@@ -4,47 +4,52 @@ import RandomConstants from '../constants/random-constants';
 import assign from 'object-assign';
 import Words from '../constants/random-words';
 
-var _word = 'fish';
+var _word = '';
 
 var CHANGE_EVENT = 'change';
 
 function setWord() {
 
-  _word = Words[Math.floor(Math.random() * Words.length)];
+    _word = Words[Math.floor(Math.random() * Words.length)];
 
 }
 
 var RandomStore = assign({}, EventEmitter.prototype, {
 
-  getWord: function() {
-    return _word;
-  },
+    getWord: function() {
 
-  addChangeListener: function(callback) {
-    this.on(CHANGE_EVENT, callback);
-  },
+        if (!_word) {
+            setWord();
+        }
 
-  removeChangeListener: function(callback) {
-    this.removeListener(CHANGE_EVENT, callback);
-  },
+        return _word;
+    },
 
-  emitChange: function() {
-    this.emit(CHANGE_EVENT);
-  }
+    addChangeListener: function(callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+
+    removeChangeListener: function(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    },
+
+    emitChange: function() {
+        this.emit(CHANGE_EVENT);
+    }
 });
 
 // Register callback to handle all updates
 AppDispatcher.register(function(action) {
 
-  switch(action.actionType) {
-    case RandomConstants.SET_WORD:
-        setWord();
-        RandomStore.emitChange();
-        break;
+    switch (action.actionType) {
+        case RandomConstants.SET_WORD:
+            setWord();
+            RandomStore.emitChange();
+            break;
 
-    default:
-      // no op
-  }
+        default:
+    // no op
+    }
 });
 
 module.exports = RandomStore;
